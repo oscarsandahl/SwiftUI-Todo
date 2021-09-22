3407f1debc8a7255a7faa80ad12e64352a3ff48e
Oscar <osandahl@deloittedigital.se>
2021-09-22 10:33:36 +0200

    First commit

---
+++ Famplan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Famplan
+//
+//  Created by Sandahl, Oscar on 2021-09-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Famplan/FamplanApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamplanApp.swift
+//  Famplan
+//
+//  Created by Sandahl, Oscar on 2021-09-22.
+//
+
+import SwiftUI
+
+@main
+struct FamplanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

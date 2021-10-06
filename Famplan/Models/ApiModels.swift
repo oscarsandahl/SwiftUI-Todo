e2635333bd5c81c4b46f60dfb3e61331356e7cef
Oscar <osandahl@deloittedigital.se>
2021-10-06 08:21:44 +0200

    create apimodel to list

---
+++ Famplan/Models/ApiModels.swift
@@ -0,0 +1,15 @@
+//
+//  ApiModels.swift
+//  Famplan
+//
+//  Created by Sandahl, Oscar on 2021-10-06.
+//
+
+import Foundation
+
+struct ListOverview: Identifiable, Decodable {
+    var id = UUID()
+    var listTitle: String
+    var image: String
+    var items: Int
+}
